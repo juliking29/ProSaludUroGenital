d1f5db7f5e7fd260dd13dab30c872fb89ed999a4
juliking29 <julichavez06@outlook.com>
2024-09-15 18:10:49 -0500

    Se añade el proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html","./FrontEnd/src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
